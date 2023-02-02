46d89207342c517d3c79baf556599f68c1086b1b
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-02-02 09:51:08 -0500

    Color constants bonification

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
+
+const primaryColorLight = Color(0xFF008AB9);
 const primaryColor = Color(0xFF124A89);
+const primaryColorDark = Color(0xFF09386D); // Use sparingly, not an official color - thin strokes...
+
+const secondaryLight = Color(0xFFF6D268); // Use sparingly, not an official color.
+const secondaryColor = Color(0xFFF1BA1B);
+const secondaryColorDark = Color(0xFFF49121);
+
+const errorColor = Color(0xFFFF4E2A);
+const errorColorLight = Color(0xFFFF8C75); // Use sparingly on primaryBackground
+const errorColorDark = Color(0xFF802614);
+
+const primaryBackground = Color(0xFFF8F8F8); // App background
+const paleText = Color(0xFFFAFAFA); // White text
+const discreetText = Color(0xFF979797); // Guidance/tooltip/disabled text
+const darkText = Color(0xFF131313); // Black text
