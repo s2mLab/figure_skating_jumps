79dc52b01b3579647e4e7e4725cc18880ab86ed7
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-02-03 22:21:46 -0500

    made the xsensconnection into a service with dart factory

--- lib/services/x_sens_dot_connection.dart
+++ lib/services/x_sens_dot_connection.dart
@@ -1,4 +1,13 @@
 class XSensDotConnection {
+  static final XSensDotConnection _xSensDotConnection = XSensDotConnection._internal();
+
+  // Dart's factory constructor allows us to get the same instance everytime this class is constructed
+  // This helps having to refer to a static class .instance attribute for every call.
+  factory XSensDotConnection() {
+    return _xSensDotConnection;
+  }
+  XSensDotConnection._internal();
+
   XSensConnectionState connectionState = XSensConnectionState.disconnected;
   final List<XSensStateSubscriber> _connectionStateSubscribers = [];
 
@@ -19,5 +28,7 @@ enum XSensConnectionState {
 }
 
 abstract class XSensStateSubscriber {
+
+  XSensConnectionState onStateChange(XSensConnectionState state);
 
 }
