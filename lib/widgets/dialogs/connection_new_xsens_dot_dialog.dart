08a8dccef9d84091b80d413c2aef122cd1275e19
Christophe St-Georges <cstg23@hotmail.ca>
2023-02-05 18:16:17 -0500

    Added basic logic for connection steps navigation

--- lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
+++ lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../../constants/colors.dart';
 
-class ConnectionNewXSensDotDialog extends StatelessWidget {
+class ConnectionNewXSensDotDialog extends StatefulWidget {
   const ConnectionNewXSensDotDialog({super.key});
+  @override
+  State<ConnectionNewXSensDotDialog> createState() => _ConnectionNewXSensDotState();
+}
+
+class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>{
+  int _connectionStep = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +19,48 @@ class ConnectionNewXSensDotDialog extends StatelessWidget {
       child: Column(
         children: [
           Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
+            width: double.infinity,
+            decoration: const BoxDecoration(
               color: primaryColorLight,
             ),
-              child: const Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 18, bottom: 18),
-                  child: Text(
-                    'Connecter un nouvel XSens DOT',
-                    style: TextStyle(color: paleText, fontSize: 20),
-                  ),
+            child: const Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 18, bottom: 18),
+                child: Text(
+                  'Connecter un nouvel XSens DOT',
+                  style: TextStyle(color: paleText, fontSize: 20),
                 ),
               ),
+            ),
           ),
-          const Text('TEST')
+          IndexedStack(
+            index: _connectionStep,
+            children: [
+              TextButton(onPressed: toVerification, child: const Text('Searching...')),
+              TextButton(onPressed: toConfiguration, child: const Text('Verifying...')),
+              TextButton(onPressed: toSearch, child: const Text('Confirmed and configured!')),
+            ],
+          )
         ],
       ),
     );
   }
-  
+
+  void toSearch(){
+    setState(() {
+      _connectionStep = 0;
+    });
+  }
+
+  void toVerification() {
+    setState(() {
+      _connectionStep = 1;
+    });
+  }
+
+  void toConfiguration() {
+    setState(() {
+      _connectionStep = 2;
+    });
+  }
 }
