1e43b3c7ffbc0bf562dc566741e2882ad83c7814
Christophe St-Georges <cstg23@hotmail.ca>
2023-02-05 17:48:47 -0500

    Completed the page title

--- lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
+++ lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class ConnectionNewXSensDotDialog extends StatelessWidget {
+  const ConnectionNewXSensDotDialog({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const SimpleDialog(
-      title: Text("TEST"),
+    return Dialog(
+      clipBehavior: Clip.antiAlias,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+      child: Column(
+        children: [
+          Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+              color: primaryColorLight,
+            ),
+              child: const Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 18, bottom: 18),
+                  child: Text(
+                    'Connecter un nouvel XSens DOT',
+                    style: TextStyle(color: paleText, fontSize: 20),
+                  ),
+                ),
+              ),
+          ),
+          const Text('TEST')
+        ],
+      ),
     );
   }
   
