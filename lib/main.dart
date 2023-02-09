a4953dcb7e3c8be5c53a4080a6c7e16b7036a364
Christophe St-Georges <cstg23@hotmail.ca>
2023-02-09 15:35:54 -0500

    Plugged the SDK in the kotlin app and made a usage example for the communication with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -44,6 +45,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const xSensChannel = MethodChannel('xsens-dot-channel');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,9 +62,45 @@ class _MyHomePageState extends State<MyHomePage> {
                       builder: (context) => const ConnectionDotView()),
                 );
               },
-              child: const Text('connection page'))
+              child: const Text('connection page')),
+          TextButton(
+              onPressed: () async {
+                _exampleXsens();
+              },
+              child: const Text('SDK connection test'))
         ],
       )),
+    );
+  }
+
+  _exampleXsens() async {
+    String first;
+    try {
+      first = await xSensChannel
+          .invokeMethod('exampleXSens', <String, dynamic>{'version': 'V1'});
+    } on PlatformException catch (e) {
+      first = "1failed";
+    }
+
+    String second;
+    try {
+      second = await xSensChannel
+          .invokeMethod('exampleXSens', <String, dynamic>{'version': 'V2'});
+    } on PlatformException catch (e) {
+      second = "2failed";
+    }
+
+    String edge;
+    try {
+      edge = await xSensChannel
+          .invokeMethod('exampleXSens', <String, dynamic>{'version': ''});
+    } on PlatformException catch (e) {
+      edge = "3failed";
+    }
+    Fluttertoast.showToast(
+      msg: ('$first $second $edge'),
+      toastLength: Toast.LENGTH_LONG,
+      fontSize: 18.0,
     );
   }
 }
