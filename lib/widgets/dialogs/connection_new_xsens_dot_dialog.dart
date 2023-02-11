d12f60e157655c90e4e4214ddd8822de82d7aa13
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-02-11 12:45:05 -0500

    Base discovery bluetooth skeleton mock for first connect dialog screen

--- lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
+++ lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
@@ -1,3 +1,6 @@
+import 'package:figure_skating_jumps/interfaces/bluetooth_discovery_subscriber.dart';
+import 'package:figure_skating_jumps/models/bluetooth_device.dart';
+import 'package:figure_skating_jumps/services/bluetooth_discovery.dart';
 import 'package:figure_skating_jumps/widgets/prompts/instruction_prompt.dart';
 import 'package:flutter/material.dart';
 
@@ -6,17 +9,29 @@ import '../../constants/colors.dart';
 class ConnectionNewXSensDotDialog extends StatefulWidget {
   const ConnectionNewXSensDotDialog({super.key});
   @override
-  State<ConnectionNewXSensDotDialog> createState() => _ConnectionNewXSensDotState();
+  State<ConnectionNewXSensDotDialog> createState() =>
+      _ConnectionNewXSensDotState();
 }
 
-class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>{
+class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>
+    implements BluetoothDiscoverySubscriber {
   int _connectionStep = 0;
+  List<BluetoothDevice> devices = [];
+  BluetoothDiscovery discoveryService = BluetoothDiscovery();
+
+  @override
+  void initState() {
+    devices = discoveryService.subscribeBluetoothDiscovery(this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      insetPadding: const EdgeInsets.all(16),
       clipBehavior: Clip.antiAlias,
-      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12))),
       child: Column(
         children: [
           Container(
@@ -26,28 +41,38 @@ class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>{
             ),
             child: const Center(
               child: Padding(
-                padding: EdgeInsets.only(top: 18, bottom: 18),
+                padding: EdgeInsets.all(16),
                 child: Text(
                   'Connecter un nouvel XSens DOT',
+                  textAlign: TextAlign.center,
                   style: TextStyle(color: paleText, fontSize: 20),
                 ),
               ),
             ),
           ),
-          IndexedStack(
-            index: _connectionStep,
-            children: [
-              TextButton(onPressed: toVerification, child: const InstructionPrompt('Veuillez donner l’autorisation à l’application d’accéder au Bluetooth. L’option se trouve généralement dans les paramètres de votre appareil.', secondaryColor)),
-              TextButton(onPressed: toConfiguration, child: const InstructionPrompt('Vérifier la réception du capteur (1/2)', secondaryColor)),
-              TextButton(onPressed: toSearch, child: const InstructionPrompt('Confirmed and configured!', secondaryColor)),
-            ],
+          Expanded(
+            child: IndexedStack(
+              index: _connectionStep,
+              children: [
+                newPairingStep(),
+                TextButton(
+                    onPressed: toConfiguration,
+                    child: const InstructionPrompt(
+                        'Vérifier la réception du capteur (1/2)',
+                        secondaryColor)),
+                TextButton(
+                    onPressed: toSearch,
+                    child: const InstructionPrompt(
+                        'Confirmed and configured!', secondaryColor)),
+              ],
+            ),
           )
         ],
       ),
     );
   }
 
-  void toSearch(){
+  void toSearch() {
     setState(() {
       _connectionStep = 0;
     });
@@ -63,5 +88,72 @@ class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>{
     setState(() {
       _connectionStep = 2;
     });
+  }
+
+  Widget newPairingStep() {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: InstructionPrompt(
+              'Veuillez donner l’autorisation à l’application d’accéder au Bluetooth. L’option se trouve généralement dans les paramètres de votre appareil.',
+              secondaryColor),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(right: 16),
+                child: CircularProgressIndicator(
+                  color: primaryColorDark,
+                  value: null,
+                ),
+              ),
+              Text('Recherche en cours', style: TextStyle(fontFamily: 'Jost'))
+            ],
+          ),
+        ),
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView.builder(
+              itemCount: devices.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child:
+                      InstructionPrompt(devices[index].name, primaryColorLight),
+                );
+              }),
+        )),
+        SizedBox(
+            height: 56,
+            width: 300,
+            child: Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      discoveryService.changeList();
+                    },
+                    child:
+                        const Text('ajouter', textAlign: TextAlign.center)))),
+        SizedBox(
+            height: 56,
+            width: 300,
+            child: Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      discoveryService.removeEntry();
+                    },
+                    child: const Text('enlever', textAlign: TextAlign.center))))
+      ],
+    );
+  }
+
+  @override
+  void onBluetoothDeviceListChange(List<BluetoothDevice> devices) {
+    this.devices = devices;
+    setState(() {});
   }
 }
