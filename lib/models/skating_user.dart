4f725b897a6dccf3c2d312cbecb1545fb59b7ad8
Christophe St-Georges <cstg23@hotmail.ca>
2023-02-13 22:20:08 -0500

    Added sign in and out functions

--- lib/models/skating_user.dart
+++ lib/models/skating_user.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:figure_skating_jumps/enums/user_role.dart';
 import 'package:figure_skating_jumps/models/capture.dart';
 
@@ -7,36 +10,46 @@ class SkatingUser {
   late String _lastName;
   late UserRole _role;
   final List<Capture> _captures = <Capture>[];
-  final List<SkatingUser> _trainees = <SkatingUser>[]; //TODO when creating skaters -> decides if whe keep a list of User or only their UID
+  final List<SkatingUser> _trainees = <
+      SkatingUser>[]; //TODO when creating skaters -> decides if whe keep a list of User or only their UID
   final List<SkatingUser> _coaches = <SkatingUser>[];
 
-  String get firstName{
+  String get firstName {
     return _firstName;
   }
 
-  String get lastName{
+  String get lastName {
     return _lastName;
   }
 
-  UserRole get role{
+  UserRole get role {
     return _role;
   }
 
-  List<Capture> get captures{
+  List<Capture> get captures {
     return _captures;
   }
 
-  List<SkatingUser> get trainees{
+  List<SkatingUser> get trainees {
     return _trainees;
   }
 
-  List<SkatingUser> get coaches{
+  List<SkatingUser> get coaches {
     return _coaches;
   }
 
-  SkatingUser(String firstName, String lastName, UserRole role) {
-    _firstName = firstName;
-    _lastName = lastName;
-    _role = role;
+  SkatingUser(this._firstName, this._lastName, this._role);
+
+  SkatingUser.fromFirestore(
+      this.uID, DocumentSnapshot<Map<String, dynamic>> userInfo) {
+    String firstName = userInfo.get('firstName');
+    String lastName = userInfo.get('lastName');
+
+    String roleStr = userInfo.get('role');
+    UserRole role = UserRole.values
+        .firstWhere((element) => element.toString() == roleStr);
+
+    SkatingUser(firstName, lastName, role);
+    //TODO convert json to list when saving acquisition and when linking trainee and coach
   }
 }
