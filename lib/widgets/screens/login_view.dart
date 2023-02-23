b36abbe4a11553376cfa66c7eb8871eadcd6e32a
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-02-23 09:54:28 -0500

    Skeleton stateful widgets - login & account creation

---
+++ lib/widgets/screens/login_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() =>
+      _LoginViewState();
+  
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
