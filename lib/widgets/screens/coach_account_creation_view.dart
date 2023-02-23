c2b82af2868d9ba33c6df59b42d57fc42e82d6f7
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-02-23 09:54:28 -0500

    Skeleton stateful widgets - login & account creation

---
+++ lib/widgets/screens/coach_account_creation_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CoachAccountCreationView extends StatefulWidget {
+  const CoachAccountCreationView({super.key});
+
+  @override
+  State<CoachAccountCreationView> createState() =>
+      _CoachAccountCreationViewState();
+
+}
+
+class _CoachAccountCreationViewState extends State<CoachAccountCreationView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
