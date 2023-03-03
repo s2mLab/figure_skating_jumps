aba3020d305d8e74677ec652aa35bc8096d1e82a
Thomas Beauchamp <thomasbeauchamp44@gmail.com>
2023-03-03 13:18:16 -0500

    fix PR

--- lib/services/x_sens_dot_channel_service.dart
+++ lib/services/x_sens_dot_channel_service.dart
@@ -63,6 +63,15 @@ class XSensDotChannelService {
     }
   }
 
+  Future<void> setRate(int rate) async {
+    try {
+      await _xSensChannel
+          .invokeMethod('setRate', <String, dynamic>{'rate': rate});
+    } on PlatformException catch (e) {
+      debugPrint(e.message);
+    }
+  }
+
   startMeasuring() async {
     try {
       debugPrint(await _xSensChannel.invokeMethod('startMeasuring'));
--- lib/services/x_sens_dot_connection.dart
+++ lib/services/x_sens_dot_connection.dart
@@ -1,13 +1,9 @@
 import 'package:figure_skating_jumps/services/x_sens_dot_channel_service.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 import '../enums/x_sens_connection_state.dart';
 import '../interfaces/i_x_sens_state_subscriber.dart';
 import '../models/bluetooth_device.dart';
 
 class XSensDotConnection {
-  static const _xSensChannel = MethodChannel('xsens-dot-channel');
   static final XSensDotConnection _xSensDotConnection =
       XSensDotConnection._internal(XSensConnectionState.disconnected);
   final List<IXSensStateSubscriber> _connectionStateSubscribers = [];
@@ -33,9 +29,10 @@ class XSensDotConnection {
   }
 
   Future<bool> connect(BluetoothDevice bluetoothDevice) async {
-    if(_currentXSensDevice == null){
-      String response = await XSensDotChannelService().connectXSensDot(macAddress: bluetoothDevice.macAddress);
-      if(response == bluetoothDevice.macAddress) {
+    if (_currentXSensDevice == null) {
+      String response = await XSensDotChannelService()
+          .connectXSensDot(macAddress: bluetoothDevice.macAddress);
+      if (response == bluetoothDevice.macAddress) {
         _currentXSensDevice = bluetoothDevice;
         _changeState(XSensConnectionState.connected);
       }
@@ -46,22 +43,13 @@ class XSensDotConnection {
   }
 
   Future<void> disconnect() async {
-    if(_currentXSensDevice != null){
+    if (_currentXSensDevice != null) {
       String response = await XSensDotChannelService().disconnectXSensDot();
       String? currentMac = _currentXSensDevice?.macAddress;
-      if(response.contains(currentMac!)) {
+      if (response.contains(currentMac!)) {
         _currentXSensDevice = null;
         _changeState(XSensConnectionState.disconnected);
       }
-    }
-  }
-
-  Future<void> setRate(int rate) async {
-    try {
-      await _xSensChannel
-          .invokeMethod('setRate', <String, dynamic>{'rate': rate});
-    } on PlatformException catch (e) {
-      debugPrint(e.message);
     }
   }
 
