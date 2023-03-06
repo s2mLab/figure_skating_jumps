f72dd2bbe0f7f2342e8f67e41ee559727e972bc7
MrPuffs <thomasbeauchamp44@gmail.com>
2023-03-06 18:08:42 -0500

    fix comment PR

--- lib/widgets/screens/login_view.dart
+++ lib/widgets/screens/login_view.dart
@@ -18,16 +18,16 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  String _coachEmail = '';
-  String _coachPassword = '';
+  String _email = '';
+  String _password = '';
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
-  final _personalInfoKey = GlobalKey<FormState>();
+  final _connectionInfoKey = GlobalKey<FormState>();
 
   @override
   void initState() {
-    _emailController = TextEditingController(text: _coachEmail);
-    _passwordController = TextEditingController(text: _coachPassword);
+    _emailController = TextEditingController(text: _email);
+    _passwordController = TextEditingController(text: _password);
     super.initState();
   }
 
@@ -35,127 +35,102 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: primaryColor,
-        body: SingleChildScrollView(
-            child: Column(
-          children: [
-            const SizedBox(height: 150),
-            Container(
-                margin: const EdgeInsets.all(32.0),
-                height: 120,
-                child: SvgPicture.asset(
-                    'assets/vectors/blanc-logo-patinage-quebec.svg')),
-            Container(
-                // height: 200,
-                width: double.infinity,
-                margin: const EdgeInsets.all(32.0),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Container(
-                    margin: const EdgeInsets.all(32.0),
+        body: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+            },
+            child: SingleChildScrollView(
+                child: Center(
                     child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 8.0),
-                          child: PageTitle(text: loginTitle),
-                        ),
-                        Form(
-                            key: _personalInfoKey,
-                            child: Column(children: [
-                              TextFormField(
-                                keyboardType: TextInputType.emailAddress,
-                                autovalidateMode:
-                                    AutovalidateMode.onUserInteraction,
-                                controller: _emailController,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _coachEmail = value;
-                                  });
+              children: [
+                Container(
+                    margin: const EdgeInsets.all(32.0),
+                    height: 100,
+                    child: SvgPicture.asset(
+                        'assets/vectors/blanc-logo-patinage-quebec.svg')),
+                Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.all(32.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Container(
+                        margin: const EdgeInsets.all(32.0),
+                        child: Column(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8.0),
+                              child: PageTitle(text: loginTitle),
+                            ),
+                            Form(
+                                key: _connectionInfoKey,
+                                child: Column(children: [
+                                  TextFormField(
+                                    keyboardType: TextInputType.emailAddress,
+                                    autovalidateMode:
+                                        AutovalidateMode.onUserInteraction,
+                                    controller: _emailController,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _email = value;
+                                      });
+                                    },
+                                    decoration: const InputDecoration(
+                                      labelText: email,
+                                      labelStyle: TextStyle(
+                                          fontSize: 16, color: discreetText),
+                                    ),
+                                  ),
+                                  TextFormField(
+                                    autovalidateMode:
+                                        AutovalidateMode.onUserInteraction,
+                                    controller: _passwordController,
+                                    obscureText: true,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _password = value;
+                                      });
+                                    },
+                                    decoration: const InputDecoration(
+                                      labelText: password,
+                                      labelStyle: TextStyle(
+                                          fontSize: 16, color: discreetText),
+                                    ),
+                                  )
+                                ])),
+                            const SizedBox(height: 32),
+                            IceButton(
+                                text: connectionButton,
+                                onPressed: () {
+                                  Navigator.pushNamed(
+                                    context,
+                                    '/',
+                                  );
                                 },
-                                validator: (value) {
-                                  return _emailValidator(value);
-                                },
-                                decoration: const InputDecoration(
-                                  labelText: email,
-                                  labelStyle: TextStyle(
-                                      fontSize: 16, color: discreetText),
-                                ),
-                              ),
-                              TextFormField(
-                                autovalidateMode:
-                                    AutovalidateMode.onUserInteraction,
-                                controller: _passwordController,
-                                obscureText: true,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _coachPassword = value;
-                                  });
-                                },
-                                validator: (value) {
-                                  return _passValidator(value);
-                                },
-                                decoration: const InputDecoration(
-                                  labelText: password,
-                                  labelStyle: TextStyle(
-                                      fontSize: 16, color: discreetText),
-                                ),
-                              )
-                            ])),
-                        const SizedBox(height: 32),
-                        IceButton(
-                            text: connectionButton,
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const GodView()),
-                              );
-                            },
-                            textColor: paleText,
-                            color: primaryColor,
-                            iceButtonImportance: IceButtonImportance.mainAction,
-                            iceButtonSize: IceButtonSize.medium),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8),
-                          child: IceButton(
-                              text: createAccount,
-                              onPressed: () {
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const CoachAccountCreationView()),
-                                );
-                              },
-                              textColor: primaryColor,
-                              color: primaryColor,
-                              iceButtonImportance:
-                                  IceButtonImportance.secondaryAction,
-                              iceButtonSize: IceButtonSize.medium),
-                        )
-                      ],
-                    ))),
-          ],
-        )));
-  }
-
-  String? _passValidator(String? value) {
-    if (value == null || value.trim().isEmpty) {
-      return pleaseFillField;
-    }
-    if (value.length < 10) {
-      return addCharacters;
-    }
-    return null;
-  }
-
-  String? _emailValidator(String? value) {
-    if (value == null || value.trim().isEmpty) {
-      return pleaseFillField;
-    }
-    if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]+$').hasMatch(value)) {
-      return invalidEmailFormat;
-    }
-    return null;
+                                textColor: paleText,
+                                color: primaryColor,
+                                iceButtonImportance:
+                                    IceButtonImportance.mainAction,
+                                iceButtonSize: IceButtonSize.medium),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8),
+                              child: IceButton(
+                                  text: createAccount,
+                                  onPressed: () {
+                                    Navigator.pushReplacementNamed(
+                                      context,
+                                      '/CoachAccountCreation',
+                                    );
+                                  },
+                                  textColor: primaryColor,
+                                  color: primaryColor,
+                                  iceButtonImportance:
+                                      IceButtonImportance.secondaryAction,
+                                  iceButtonSize: IceButtonSize.medium),
+                            )
+                          ],
+                        ))),
+              ],
+            )))));
   }
 }
