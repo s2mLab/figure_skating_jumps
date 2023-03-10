c162687f87747be30679e691c71a61caa4cc6fac
Thomas Beauchamp <thomasbeauchamp44@gmail.com>
2023-03-10 17:17:10 -0500

    tab navigation

---
+++ lib/widgets/layout/options_tab.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OptionsTab extends StatefulWidget {
+  const OptionsTab({Key? key}) : super(key: key);
+
+  @override
+  _OptionsTabState createState() => _OptionsTabState();
+}
+
+class _OptionsTabState extends State<OptionsTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('option'),
+    );
+  }
+}
