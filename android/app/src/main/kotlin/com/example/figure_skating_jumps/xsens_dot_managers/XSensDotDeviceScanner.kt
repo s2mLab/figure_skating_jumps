59cc10566b9f47e38feddb1d5949b5d06ca52a5f
Christophe St-Georges <cstg23@hotmail.ca>
2023-03-14 23:12:59 -0500

    Corrected scan and change from LF to CRLF in kotlin

--- android/app/src/main/kotlin/com/example/figure_skating_jumps/xsens_dot_managers/XSensDotDeviceScanner.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/xsens_dot_managers/XSensDotDeviceScanner.kt
@@ -7,29 +7,22 @@ import android.bluetooth.le.ScanSettings
 import android.content.Intent
 import android.util.Log
 import com.example.figure_skating_jumps.MainActivity
+import com.example.figure_skating_jumps.channels.event_channels.XSensDotScanStreamHandler
 import com.xsens.dot.android.sdk.interfaces.XsensDotScannerCallback
 import com.xsens.dot.android.sdk.utils.XsensDotScanner
 
 class XSensDotDeviceScanner(mainActivity: MainActivity) : XsensDotScannerCallback {
     private var mainActivity: MainActivity? = mainActivity
     private var mXsScanner: XsensDotScanner = XsensDotScanner(mainActivity, this)
-    private var devicesInfo = mutableListOf<Pair<String?, String?>>()
 
     override fun onXsensDotScanned(device: BluetoothDevice?, rssi: Int) {
-        if (device?.address != null  && !devicesInfo.any {it.first == device.address }) {
-            try {
-                devicesInfo.add(Pair(device.address, device.alias ))
-            }
-            catch (e: SecurityException) {
-                Log.e("Android", e.message!!)
-            }
+        if (device?.address != null) {
+                XSensDotScanStreamHandler.sendEvent(device)
         }
     }
 
     fun startScan() {
-        devicesInfo.clear()
-        mXsScanner.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-
+        //TODO remove when requesting bluetooth permissions
         val activity = mainActivity?.context as Activity
 
         val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
@@ -40,13 +33,11 @@ class XSensDotDeviceScanner(mainActivity: MainActivity) : XsensDotScannerCallbac
         catch (e: SecurityException) {
             Log.e("Android", e.message!!)
         }
+        mXsScanner.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
         mXsScanner.startScan()
     }
 
-    fun stopScan(): MutableList<Pair<String?, String?>> {
+    fun stopScan() {
         mXsScanner.stopScan()
-
-        // return listDevice
-        return devicesInfo
     }
 }
