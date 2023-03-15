327467e249fbf9c37b11ef29da8b90cc8ebdea18
MrPuffs <thomasbeauchamp44@gmail.com>
2023-03-15 17:20:23 -0400

    Update options_tab.dart

--- lib/widgets/layout/options_tab.dart
+++ lib/widgets/layout/options_tab.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 
-class OptionsTab extends StatefulWidget {
+class OptionsTab extends StatelessWidget {
   const OptionsTab({Key? key}) : super(key: key);
 
-  @override
-  State<OptionsTab> createState() {
-    return _OptionsTabState();
-  }
-}
-
-class _OptionsTabState extends State<OptionsTab> {
   @override
   Widget build(BuildContext context) {
     return const Center(
