a2501dd2206b0a857647eea02c3cfe7c678ec2f6
Christophe St-Georges <cstg23@hotmail.ca>
2023-03-19 23:40:34 -0500

    Added the 120Hz to the recording

--- android/app/src/main/kotlin/com/example/figure_skating_jumps/x_sens_dot/enums/RecordingStatus.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/x_sens_dot/enums/RecordingStatus.kt
@@ -1,6 +1,7 @@
 package com.example.figure_skating_jumps.x_sens_dot.enums
 
 enum class RecordingStatus(val status: String) {
+    SetRate("SetRate"),
     EnableRecordingNotificationDone("EnableRecordingNotificationDone"),
     RecordingStarted("RecordingStarted"),
     RecordingStopped("RecordingStopped"),
