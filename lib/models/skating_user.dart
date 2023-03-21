5ab750ea253b7a8ad66a0ecce21476939a6bd09b
MrPuffs <thomasbeauchamp44@gmail.com>
2023-03-21 14:28:21 -0400

    get list as skatinguser

--- lib/models/skating_user.dart
+++ lib/models/skating_user.dart
@@ -2,13 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:figure_skating_jumps/enums/user_role.dart';
 
 class SkatingUser {
+  static const String _userCollectionString = 'users';
+
   late String? uID;
   late String _firstName;
   late String _lastName;
   late UserRole _role;
   late String _email;
   late List<String> _captures = [];
-  late List<String> _trainees = [];
+  late List<String> _traineesID = [];
+  late List<SkatingUser> _trainees = [];
   final List<String> _coaches = [];
 
   String get firstName {
@@ -31,12 +34,26 @@ class SkatingUser {
     return _captures;
   }
 
-  List<String> get trainees {
+  List<SkatingUser> get trainees {
     return _trainees;
+  }
+
+  List<String> get traineesID {
+    return _traineesID;
   }
 
   List<String> get coaches {
     return _coaches;
+  }
+
+  loadTrainees() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    _trainees = [];
+    for (String id in _traineesID) {
+      SkatingUser trainee = SkatingUser.fromFirestore(
+          id, await firestore.collection(_userCollectionString).doc(id).get());
+      _trainees.add(trainee);
+    }
   }
 
   SkatingUser(this._firstName, this._lastName, this._role, [this.uID]);
@@ -52,7 +69,8 @@ class SkatingUser {
     SkatingUser skaterUser = SkatingUser(firstName, lastName, role, uID);
 
     skaterUser._captures = List<String>.from(userInfo.get('captures') as List);
-    skaterUser._trainees = List<String>.from(userInfo.get('trainees') as List);
+    skaterUser._traineesID =
+        List<String>.from(userInfo.get('trainees') as List);
     return skaterUser;
     //TODO convert json to list when saving acquisition and when linking trainee and coach
   }
