6d8a781b7672684fbf1ff705cb746ab991131c8b
cravethedave <d.saikali@live.com>
2023-03-24 16:45:51 -0400

    Working code with comments

--- python_analysis/analysis.py
+++ python_analysis/analysis.py
@@ -1,101 +1,312 @@
 import pandas as pd
 import numpy as np
 import time
+import analysis_utils
+import click
 
-from math import sin, cos, radians, sqrt
+from enum import Enum
+from math import sqrt
+
 
 # Referential while XSens faces you and is right way up
 # X goes up
 # Y goes left
 # Z goes towards you
 
-def read_csv(source) -> pd.DataFrame:
-    data: pd.DataFrame = pd.read_csv(
-        source,
-        header=0,
-        names=('counter', 'time', 'euler_x', 'euler_y', 'euler_z', 'acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z')
-    )
-    return data
 
-def make_data_abs(data: pd.DataFrame) -> pd.DataFrame:
-    abs_data = []
-    for iter, row in data.iterrows():
-        # Apply referential rotation to work with new axes
-        rot_mat = get_rotation_matrix(row['euler_x'], row['euler_y'], row['euler_z'])
-        acc = np.array([row['acc_x'], row['acc_y'], row['acc_z']])
-        acc = np.matmul(rot_mat, acc)
-        gyr = np.array([row['gyr_x'], row['gyr_y'], row['gyr_z']])
-        gyr = np.matmul(rot_mat, gyr)
-        
-        abs_data.append([iter, acc[0], acc[1], acc[2], gyr[0], gyr[1], gyr[2]])
-    return pd.DataFrame(abs_data, columns=['iter', 'acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z'])
+SEC_TO_MS_RATIO = 10**6 # conversion from microseconds to seconds
+MAX_NOISE = 3.5 # meters per second squared
+MIN_FREE_FALL_TIME = 0.2 * SEC_TO_MS_RATIO # 0.3 seconds in microseconds
+MAX_INTERRUPTION = 0.05 * SEC_TO_MS_RATIO # 0.03 seconds in microseconds
+MIN_ROTATION_SPEED = 480 # degrees per second
+MIN_IMPULSE = 20 # meters per second squared
 
-def get_rotation_matrix(x_deg, y_deg, z_deg) -> np.ndarray:
-    x = radians(x_deg)
-    y = radians(y_deg)
-    z = radians(z_deg)
-    rot_z = np.array([[cos(z), -sin(z), 0], [sin(z), cos(z), 0], [0, 0, 1]])
-    rot_y = np.array([[cos(y), 0, sin(y)], [0, 1, 0], [-sin(y), 0, cos(y)]])
-    rot_x = np.array([[1, 0, 0], [0, cos(x), -sin(x)], [0, sin(x), cos(x)]])
-    return np.matmul(np.matmul(rot_z, rot_y), rot_x)
-
-def find_jumps(data: pd.DataFrame) -> "list[pd.DataFrame]":
-    # Define constants for the math
-    DELTA_T: int = data.iloc[1]['time'] - data.iloc[0]['time']
-    MIN_FREE_FALL_TIME: int = 0.3 * 10**6 # 0.3 seconds in microseconds
-    MAX_NOISE: int = 3
-    WINDOW_TIME: int = 0.2 * 10**6 # 0.2 seconds in microseconds
-    WINDOW_TICK: int = WINDOW_TIME // DELTA_T # window time in ticks
-    detection_ticks = 0
-    fall_ticks = 0
-    jumps = []
+class Jump:
+    start: int
+    end: int
     
+    def __init__(self, start: int, end: int) -> None:
+        self.start = start
+        self.end = end
+    
+class Jump_Analysis:
+    start: int
+    end: int
+    duration: float
+    num_rot: float
+    max_rot_speed: float
+    time_to_max: float
+    
+    def __init__(self, start, end, num_rot, max_rot_speed, time_to_max) -> None:
+        self.start = start
+        self.end = end
+        self.duration = (end - start) * DELTA_T / SEC_TO_MS_RATIO
+        self.num_rot = num_rot
+        self.max_rot_speed = max_rot_speed
+        self.time_to_max = time_to_max
+    
+    def toMap(self) -> map:
+        return {
+            "start": self.start,
+            "end": self.end,
+            "duration": self.duration,
+            "numRot": self.num_rot,
+            "maxRotSpeed": self.max_rot_speed,
+            "timeToMax": self.time_to_max
+        }
+    
+class State(Enum):
+    SKATING = 0
+    DETECTING = 1
+    FALLING = 2
+
+def find_jumps_from_vertical_acc(data: pd.DataFrame) -> 'list[Jump]':
+    """Uses the vertical acceleration of each frame to determine if it is a jump or not.
+    It looks for a period of time where the vertical acceleration is close to zero.
+    It allows small interruptions during which the vertical acceleration can go 
+    up/down without disqualifying the jump.
+
+    Args:
+        data (pd.DataFrame)
+
+    Returns:
+        list[Jump]
+    """
+    jumps: list[Jump] = []
+    state: State = State.SKATING
+    jump_start: int = 0
+    falling_ticks: int = 0
+    interruption_ticks: int = 0
     # Find the jumps
     for iter, row in data.iterrows():
-        rot_mat = get_rotation_matrix(row['euler_x'], row['euler_y'], row['euler_z'])
         acc = np.array([row['acc_x'], row['acc_y'], row['acc_z']])
-        acc = np.matmul(rot_mat, acc)
-        is_noise = abs(acc[2]) < MAX_NOISE
+        free_falling = abs(acc[2]) < MAX_NOISE
         
-        # Increase length of detection zone
-        if is_noise:
-            detection_ticks += 1
-        else:
-            detection_ticks = 0
-        
-        # If it has found a fall, keeps counting until it finds no more
-        if fall_ticks > 0:
-            if is_noise:
-                fall_ticks += 1
+        if (state == State.SKATING):
+            # If we detect a fall, pass to Detecting and increase counter
+            if free_falling:
+                falling_ticks += 1
+                jump_start = iter
+                state = State.DETECTING
+                
+        elif (state == State.DETECTING):
+            # Increase the counters to see if we detect a fall or if it was fake
+            if free_falling:
+                falling_ticks += 1
+                interruption_ticks = 0
             else:
-                jumps.append({'start': int(iter - fall_ticks - WINDOW_TICK), 'end': int(iter + WINDOW_TICK)})
-                fall_ticks = 0
-        # If there is no fall, it will start one
-        elif detection_ticks * DELTA_T >= MIN_FREE_FALL_TIME:
-            fall_ticks = detection_ticks
+                interruption_ticks += 1
+            
+            # Make a decision if one of the counters reached a critical point
+            if interruption_ticks * DELTA_T >= MAX_INTERRUPTION:
+                falling_ticks = 0
+                interruption_ticks = 0
+                state = State.SKATING
+            elif falling_ticks * DELTA_T >= MIN_FREE_FALL_TIME:
+                state = State.FALLING
+                
+        elif (state == State.FALLING):
+            if free_falling:
+                falling_ticks += 1
+                interruption_ticks = 0
+            else:
+                interruption_ticks += 1
+                
+            if interruption_ticks * DELTA_T >= MAX_INTERRUPTION:
+                jumps.append(Jump(jump_start, iter))
+                falling_ticks = 0
+                interruption_ticks = 0
+                state = State.SKATING
     
-    # Merges jumps that are very close to avoid a single wrong value ruining the result
+    return jumps
+
+def find_jumps_from_rotation_speed(data: pd.DataFrame) -> 'list[Jump]':
+    """Uses the rotation speed of each frame to determine if it is a jump or not.
+    It looks for a period of time where the norm of the rotation speed vector is high enough.
+    It allows small interruptions during which the speed can go back down without 
+    disqualifying the jump. It also needs at least one moment during which it detects a freefall
+    for it to count as a jump.
+
+    Args:
+        data (pd.DataFrame)
+
+    Returns:
+        list[Jump]
+    """
+    jumps: list[Jump] = []
+    state: State = State.SKATING
+    jump_start: int = 0
+    spinning_ticks: int = 0
+    interruption_ticks: int = 0
+    free_falling = False
+    
+    for iter, row in data.iterrows():
+        acc = np.array([row['acc_x'], row['acc_y'], row['acc_z']])
+        gyr = np.array([row['gyr_x'], row['gyr_y'], row['gyr_z']])
+        spinning = np.linalg.norm(gyr) >= MIN_ROTATION_SPEED
+        
+        if (state == State.SKATING):
+            # If we detect a spin, pass to Detecting and increase counter
+            if spinning:
+                spinning_ticks += 1
+                jump_start = iter
+                state = State.DETECTING
+        elif (state == State.DETECTING):
+            # Increase the counters to see if we detect a fall or if it was fake
+            if spinning:
+                free_falling = abs(acc[2]) < MAX_NOISE or free_falling
+                spinning_ticks += 1
+                interruption_ticks = 0
+            else:
+                interruption_ticks += 1
+            
+            # Make a decision if one of the counters reached a critical point
+            if interruption_ticks * DELTA_T >= MAX_INTERRUPTION:
+                spinning_ticks = 0
+                interruption_ticks = 0
+                free_falling = False
+                state = State.SKATING
+            elif spinning_ticks * DELTA_T >= MIN_FREE_FALL_TIME and free_falling:
+                state = State.FALLING
+        elif (state == State.FALLING):
+            if spinning:
+                spinning_ticks += 1
+                interruption_ticks = 0
+            else:
+                interruption_ticks += 1
+            
+            if interruption_ticks * DELTA_T >= MAX_INTERRUPTION:
+                jumps.append(Jump(jump_start, iter))
+                spinning_ticks = 0
+                interruption_ticks = 0
+                free_falling = False
+                state = State.SKATING
+    
+    return jumps
+
+def find_jumps_from_horizontal_acc(data: pd.DataFrame) -> 'list[Jump]':
+    """Uses the horizontal acceleration of each frame to determine if it is a jump or not.
+    It looks for a period of time where the norm of the horizontal acceleration vector is high enough.
+    It allows small interruptions during which the acceleration can go back down without 
+    disqualifying the jump. It also needs at least one moment during which it detects a freefall
+    for it to count as a jump.
+
+    Args:
+        data (pd.DataFrame)
+
+    Returns:
+        list[Jump]
+    """
+    jumps: list[Jump] = []
+    state: State = State.SKATING
+    jump_start: int = 0
+    impulse_ticks: int = 0
+    interruption_ticks: int = 0
+    free_falling = False
+    
+    for iter, row in data.iterrows():
+        acc = np.array([row['acc_x'], row['acc_y'], row['acc_z']])
+        acc_xy = np.linalg.norm(acc[:1])
+        is_impulse_moment = acc_xy >= MIN_ROTATION_SPEED
+        
+        if (state == State.SKATING):
+            # If we detect a spin, pass to Detecting and increase counter
+            if is_impulse_moment:
+                impulse_ticks += 1
+                jump_start = iter
+                state = State.DETECTING
+        elif (state == State.DETECTING):
+            # Increase the counters to see if we detect a fall or if it was fake
+            if is_impulse_moment:
+                free_falling = abs(acc[2]) < MAX_NOISE or free_falling
+                impulse_ticks += 1
+                interruption_ticks = 0
+            else:
+                interruption_ticks += 1
+            
+            # Make a decision if one of the counters reached a critical point
+            if interruption_ticks * DELTA_T >= MAX_INTERRUPTION:
+                impulse_ticks = 0
+                interruption_ticks = 0
+                free_falling = False
+                state = State.SKATING
+            elif impulse_ticks * DELTA_T >= MIN_FREE_FALL_TIME and free_falling:
+                state = State.FALLING
+        elif (state == State.FALLING):
+            if is_impulse_moment:
+                impulse_ticks += 1
+                interruption_ticks = 0
+            else:
+                interruption_ticks += 1
+            
+            if interruption_ticks * DELTA_T >= MAX_INTERRUPTION:
+                jumps.append(Jump(jump_start, iter))
+                impulse_ticks = 0
+                interruption_ticks = 0
+                free_falling = False
+                state = State.SKATING
+    
+    return jumps
+
+def find_jumps(data: pd.DataFrame) -> 'list[Jump]':
+    """Finds the jumps in the given dataset. It uses three metrics to do so:
+    vertical acceleration, horizontal acceleration and rotation speed.
+    It then merges the results into one array that it returns.
+
+    Args:
+        data (pd.DataFrame)
+
+    Returns:
+        list[Jump]: A list of jumps, containing their start and end frame.
+    """
+    # Find all jumps
+    jumps: list[Jump] = find_jumps_from_vertical_acc(data)
+    jumps.extend(find_jumps_from_rotation_speed(data))
+    jumps.extend(find_jumps_from_horizontal_acc(data))
+    
+    # Sort them to allow for a merge
+    jumps.sort(key=lambda jump: (jump.start))
+    
+    # Merges jumps that are overlapping to avoid a single wrong value ruining the result
     index_to_remove = []
     for i in range(len(jumps) - 1):
-        if jumps[i]['end'] >= jumps[i + 1]['start']:
-            jumps[i]['end'] = jumps[i + 1]['end']
+        if jumps[i].end >= jumps[i + 1].start:
+            jumps[i].end = jumps[i + 1].end
             index_to_remove.append(i+1)
     
-    # Flips the array so we can remove without affecting the other indexes
-    index_to_remove.reverse()
-    for iter in index_to_remove:
+    # Flips the array so we can remove redundant jumps without affecting the other indexes
+    for iter in reversed(index_to_remove):
         del jumps[iter]
     
     return jumps
 
-def analyze_session(source):
-    # Read from the csv
-    data: pd.DataFrame = read_csv(source)
-    jumps = find_jumps(data)
+def find_num_rot(jump_data: pd.DataFrame) -> float:
+    """Finds the number of rotations from a jump's data.
 
-def analyze_jump(jump_data: pd.DataFrame):
-    max_rot_speed = 0 # return in a struct
-    time_to_max = 0 # return in a struct
+    Args:
+        jump_data (pd.DataFrame)
+
+    Returns:
+        float: The number of rotations done in degrees.
+    """
+    return sum([
+        np.linalg.norm(np.array([row['gyr_x'],row['gyr_y'],row['gyr_z']]))
+        for _, row in jump_data.iterrows()
+    ]) * (DELTA_T / SEC_TO_MS_RATIO)
+
+def analyze_jump(jump: Jump, jump_data: pd.DataFrame) -> Jump_Analysis:
+    """Analyzes a single jump using the jump and the data set.
+
+    Args:
+        jump (Jump)
+        jump_data (pd.DataFrame): The data of the current jump only. Not the whole data set.
+
+    Returns:
+        Jump_Analysis
+    """
+    num_rot = find_num_rot(jump_data)
+    max_rot_speed = 0
+    time_to_max = 0
     rot_speed = 0
     for _, row in jump_data.iterrows():
         rot_vec = np.array([row['gyr_x'], row['gyr_y'], row['gyr_z']])
@@ -105,25 +316,35 @@ def analyze_jump(jump_data: pd.DataFrame):
             time_to_max = row['iter']
     
     time_to_max -= jump_data.iloc[0]['iter']
+    time_to_max *= (8333 / SEC_TO_MS_RATIO)
+    return Jump_Analysis(jump.start, jump.end, num_rot, max_rot_speed, time_to_max)
+
+@click.command()
+@click.argument("source", type=str)
+def analyze_session(source: str) -> 'list[Jump_Analysis]':
+    """Reads a raw XSensDot output file and analyzes it.
+
+    Args:
+        source (str): The path to the file to read from.
+
+    Returns:
+        list[Jump_Analysis]: A list of all jump analysis.
+    """
+    # Read from the csv
+    data: pd.DataFrame = analysis_utils.read_raw_csv_to_dataframe(source)
     
-    print(time_to_max * 8333 / (10 ** 6), max_rot_speed)
+    # Assign DELTA_T as soon as we can
+    global DELTA_T
+    DELTA_T = data.iloc[1]['time'] - data.iloc[0]['time']
+    
+    # Make data absolute and analyze it
+    data = analysis_utils.make_data_abs(data)
+    jumps: list[Jump] = find_jumps(data)
+    jump_analysis: list[Jump_Analysis] = []
+    for j in jumps:
+        jump_analysis.append(analyze_jump(j, data.iloc[j.start:j.end+1]))
+    
+    return jump_analysis
 
-def find_num_rot(jump_data: pd.DataFrame, DELTA_T = 8333):
-    return sum([sqrt(sum(np.array([row['gyr_x'],row['gyr_y'],row['gyr_z']])**2)) * (DELTA_T / 10**6) for _, row in jump_data.iterrows()])
-
-def caller(source):
-    data = make_data_abs(read_csv(source))
-    print(find_num_rot(data.iloc[2089:2193]))
-
-start = time.time()
-# caller("anthony/abs_01.csv", 2975, 3013)
-caller("anthony/any_04.csv")
-# trajectory = find_trajectory("./face_up.csv")
-# find_jumps("first")
-# jumps = find_jumps_official("./anthony/any_01.csv")
-# print(jumps)
-# jumps = find_jumps_experimental("./anthony/any_02.csv")
-# print(jumps)
-# find_jumps("./spin.csv")
-print(f"Finished in {time.time() - start} seconds.")
-# show_graph(trajectory)
+if __name__ == "__main__":
+    analyze_session()
--- python_analysis/graph.py
+++ python_analysis/graph.py
@@ -2,11 +2,19 @@ import click
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import analysis_utils
 
 from math import sqrt
-from analysis import read_csv
 
-def read_abs_csv(source) -> pd.DataFrame:
+def read_abs_csv(source: str) -> pd.DataFrame:
+    """Reads an absolute csv into a dataframe.
+
+    Args:
+        source (str): The path to the file to read from.
+
+    Returns:
+        pd.DataFrame: The dataframe containing all the data.
+    """
     data: pd.DataFrame = pd.read_csv(
         source,
         header=0,
@@ -14,96 +22,70 @@ def read_abs_csv(source) -> pd.DataFrame:
     )
     return data
 
-def add_acc_z(data: pd.DataFrame):
-    plt.plot(data['iter'].to_list(), data[f'acc_z'].to_list())
-    plt.title('Vertical Acceleration')
-
-def add_acc_xy(data: pd.DataFrame):
-    acc_xy = [
-        sqrt(row['acc_x'] ** 2 + row['acc_y'] ** 2) 
-        for iter, row in data.iterrows()
-    ]
-    plt.plot(data['iter'].to_list(), acc_xy)
-    plt.title('Horizontal Acceleration')
-
-def add_rot_speed(data: pd.DataFrame):
-    rot = [
-        sqrt(row['gyr_x'] ** 2 + row['gyr_y'] ** 2 + row['gyr_z'] ** 2)
-        for iter, row in data.iterrows()
-    ]
-    plt.plot(data['iter'].to_list(), rot)
-    plt.title('Rotation Speed')
-
-def show_acc_z(source: str, is_abs: bool = True):
-    data = read_abs_csv(source) if is_abs else read_csv(source)
-    add_acc_z(data)
-    plt.show()
-    
-def show_acc_xy(source: str, is_abs: bool = True):
-    data = read_abs_csv(source) if is_abs else read_csv(source)
-    add_acc_xy()
-    plt.show()
-
-def show_rot_speed(source: str, is_abs: bool = True):
-    data = read_abs_csv(source) if is_abs else read_csv(source)
-    add_rot_speed(data)
-    plt.show()
-
 @click.command()
-@click.argument("source", type=click.File('r'))
-@click.argument("is_abs", type=bool, required=False)
-def show_all_data(source: str, is_abs: bool = True):
+@click.argument("source", type=str)
+@click.option("-a", "--is-abs", is_flag=True, default=False, help="Include this flag if csv is treated to be absolute.")
+@click.option("-h", "--height", default=5, type=int, help="The height of the graph.")
+@click.option("-w", "--width", default=10, type=int, help="The width of the graph.")
+def show_treated_data(source: str, is_abs: bool = False, height: int = 5, width: int = 10) -> None:
     """Shows the three metrics used to find jumps.
 
     Args:
-        source (str): The file to read from.
-        is_abs (bool, optional): Determines wether the file was treated
-        to be absolute or not. Defaults to True.
+        source (str): The file to read from.\n
+        is_abs (bool): Whether the csv is absolute.\n
+        height (int): The height of the graph.\n
+        width (int): The width of the graph.
     """
-    data = read_abs_csv(source) if is_abs else read_csv(source)
-    max_x = data['iter'].to_list()[-1]
-    increment = 100
+    # Reads the csv and loads it into a dataframe
+    data: pd.DataFrame
+    if is_abs:
+        data = read_abs_csv(source)
+    else:
+        data = analysis_utils.make_data_abs(analysis_utils.read_raw_csv_to_dataframe(source))
+    
+    # Creates the figure as three superposed graphs sharing the x lines    
     fig = plt.figure()
     grid_spec = fig.add_gridspec(3, hspace=0)
     axis: list[plt.Axes] = grid_spec.subplots(sharex=True)
     
-    axis[0].plot(data['iter'].to_list(), data[f'acc_z'].to_list())
+    # Adds the vertical acceleration graph
+    axis[0].plot(data['iter'].to_list(), data['acc_z'].to_list())
     axis[0].set_ylabel('Vertical Acceleration')
-    axis[0].axhline(y=0, color='k')
+    axis[0].axhline(y=0, color='k') # Helps to see when there is a free fall
     
+    # Calculates and adds the horizontal acceleration graph
     acc_xy = [
-        sqrt(row['acc_x'] ** 2 + row['acc_y'] ** 2) 
+        np.linalg.norm(np.array([row['acc_x'], row['acc_y']]))
         for _, row in data.iterrows()
     ]
     axis[1].plot(data['iter'].to_list(), acc_xy)
     axis[1].set_ylabel('Horizontal Acceleration')
-    axis[1].set_ylim(bottom=0)
+    axis[1].set_ylim(bottom=0) # The values are never negative
     
+    # Calculates and adds the rotation speed graph
     rot = [
-        sqrt(row['gyr_x'] ** 2 + row['gyr_y'] ** 2 + row['gyr_z'] ** 2)
+        np.linalg.norm(np.array([row['gyr_x'], row['gyr_y'], row['gyr_z']]))
         for _, row in data.iterrows()
     ]
     axis[2].plot(data['iter'].to_list(), rot)
     axis[2].set_ylabel('Rotation Speed')
-    axis[2].set_ylim(bottom=0)
+    axis[2].set_ylim(bottom=0) # The values are never negative
     
+    # Prevents the x axis from going past the data on all graphs
+    # Makes the increment smaller to make it more readable
+    increment: int = 100
+    max_x: int = data['iter'].to_list()[-1]
     for iter in axis:
         iter.set_xticks(np.arange(0, max_x, increment))
         iter.set_xlim(left=0, right=max_x)
         iter.grid(axis='x')
-        
     plt.xticks(fontsize=8)
-    fig.set_figwidth(16)
-    fig.set_figheight(8)
+    
+    # Makes the graph bigger on the screen
+    fig.set_figwidth(width)
+    fig.set_figheight(height)
     fig.tight_layout()
     plt.show()
 
-@click.group()
-def graph():
-    """Run --help with the command name to get more information"""
-    pass
-
-graph.add_command(show_all_data)
-
 if __name__ == "__main__":
-    graph()
+    show_treated_data()
