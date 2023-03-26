6c25a0bea16359de73b35a6b7d4f4fc44dc6dc69
Christophe St-Georges <cstg23@hotmail.ca>
2023-03-26 16:50:44 -0500

    Made basic chart for acceleration

--- lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
+++ lib/widgets/dialogs/connection_new_xsens_dot_dialog.dart
@@ -12,6 +12,7 @@ import 'package:figure_skating_jumps/widgets/buttons/x_sens_dot_list_element.dar
 import 'package:figure_skating_jumps/widgets/icons/x_sens_state_icon.dart';
 import 'package:figure_skating_jumps/widgets/prompts/instruction_prompt.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../../constants/colors.dart';
 import '../../constants/lang_fr.dart';
@@ -26,12 +27,34 @@ class ConnectionNewXSensDotDialog extends StatefulWidget {
 
 class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>
     implements IBluetoothDiscoverySubscriber {
-  // static const int defaultFrequency = 60; waiting Christophe MR to override comment
+  final List<_DummyTempSkatingData> _data = [
+    _DummyTempSkatingData('23/02', 35),
+    _DummyTempSkatingData('27/02', 28),
+    _DummyTempSkatingData('7/03', 34),
+    _DummyTempSkatingData('8/03', 32),
+    _DummyTempSkatingData('16/03', 40)
+  ];
+  final List<_DummyTempSkatingData> _data2 = [
+    _DummyTempSkatingData('23/02', 15),
+    _DummyTempSkatingData('27/02', 18),
+    _DummyTempSkatingData('7/03', 44),
+    _DummyTempSkatingData('8/03', 12),
+    _DummyTempSkatingData('16/03', 10)
+  ];
 
+  final List<_DummyTempSkatingData> _data3 = [
+    _DummyTempSkatingData('23/02', 25),
+    _DummyTempSkatingData('27/02', 22),
+    _DummyTempSkatingData('7/03', 24),
+    _DummyTempSkatingData('8/03', 21),
+    _DummyTempSkatingData('16/03', 27)
+  ];
   int _connectionStep = 0;
   List<BluetoothDevice> _devices = [];
   final BluetoothDiscovery _discoveryService = BluetoothDiscovery();
-  final XSensDotConnectionService _xSensDotConnectionService = XSensDotConnectionService();
+  final XSensDotConnectionService _xSensDotConnectionService =
+      XSensDotConnectionService();
+
   //final XSensDotChannelService _xSensDotChannelService = XSensDotChannelService(); waiting Christophe MR to override comment
 
   @override
@@ -158,7 +181,7 @@ class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>
         const Padding(
           padding: EdgeInsets.all(16),
           child: Center(
-            //TODO connecting before changing lists in top XSENS button
+              //TODO connecting before changing lists in top XSENS button
               child: XSensStateIcon(false, XSensDeviceState.connecting)),
         ),
         const Padding(
@@ -166,18 +189,47 @@ class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>
           child: InstructionPrompt(verifyConnectivity, secondaryColor),
         ),
         Expanded(
-            child: Container(
-          color: Colors.pink,
-          height: 60,
-          width: 300,
-        )),
+          child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              primaryYAxis: NumericAxis(decimalPlaces: 2, rangePadding: ChartRangePadding.round),
+              // Chart title
+              title: ChartTitle(
+                  text: 'Acceleration',
+                  textStyle: const TextStyle(fontSize: 12.0, fontFamily: 'Jost'),
+                  alignment: ChartAlignment.near),
+              // Enable legend
+              legend: Legend(
+                  isVisible: true,
+                  position: LegendPosition.right,
+                  alignment: ChartAlignment.center,
+                  textStyle: const TextStyle(fontSize: 8.0, fontFamily: 'Jost'),
+                  itemPadding: 0.0),
+              series: <ChartSeries<_DummyTempSkatingData, String>>[
+                FastLineSeries<_DummyTempSkatingData, String>(
+                    dataSource: _data,
+                    xValueMapper: (_DummyTempSkatingData data, _) => data.year,
+                    yValueMapper: (_DummyTempSkatingData data, _) => data.score,
+                    name: 'X',
+                    width: 1),
+                FastLineSeries<_DummyTempSkatingData, String>(
+                    dataSource: _data2,
+                    xValueMapper: (_DummyTempSkatingData data, _) => data.year,
+                    yValueMapper: (_DummyTempSkatingData data, _) => data.score,
+                    name: 'Y',
+                    width: 1),
+                FastLineSeries<_DummyTempSkatingData, String>(
+                    dataSource: _data3,
+                    xValueMapper: (_DummyTempSkatingData data, _) => data.year,
+                    yValueMapper: (_DummyTempSkatingData data, _) => data.score,
+                    name: 'Z',
+                    width: 1),
+              ]),
+        ),
         Padding(
           padding: const EdgeInsets.only(top: 16, bottom: 16),
           child: IceButton(
               text: completePairing,
-              // TODO: Forbid if frequency hasn't been chosen
               onPressed: () {
-                // TODO: Send the configuration to the device.
                 Navigator.pop(context);
               },
               textColor: paleText,
@@ -216,9 +268,9 @@ class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>
   //TODO: Important to understand that this is always a new device, e.g. it isn't already in the users known device list
   //TODO: That check will have to be implemented
 
+  //TODO: Maybe change for connection event when they will be done
   Future<void> _onDevicePressed(BluetoothDevice device) async {
-    if (await _xSensDotConnectionService.connect(device) /*&&
-        await _xSensDotChannelService.setRate(defaultFrequency)*/) { //waiting Christophe MR to override comment
+    if (await _xSensDotConnectionService.connect(device)) {
       //TODO: await UserPreferenceManager().addDeviceToKnown(device.macAddress);
       setState(() {
         _connectionStep = 1;
@@ -226,4 +278,11 @@ class _ConnectionNewXSensDotState extends State<ConnectionNewXSensDotDialog>
     }
     //TODO show error message (when I will have the UI model to do so)
   }
+}
+
+class _DummyTempSkatingData {
+  _DummyTempSkatingData(this.year, this.score);
+
+  final String year;
+  final double score;
 }
