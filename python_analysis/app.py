093f5c16a6aa034abfa3d4c3225a46b61e8e19f4
cravethedave <d.saikali@live.com>
2023-03-25 23:10:06 -0400

    Server working for file adding

--- python_analysis/app.py
+++ python_analysis/app.py
@@ -1,7 +1,9 @@
-from analysis import analyze_session
-from os.path import exists
+import csv
 
-from flask import Flask, request, jsonify
+from analysis import analyze_session
+from ice_exceptions import EmptyFileError, InvalidFormatError
+
+from flask import Flask, request
 from flask_api import status
 # from app import app
 
@@ -17,11 +19,42 @@ def test():
 def analyze():
     try:
         fileName: str = request.json['fileName']
-        return jsonify(analyze_session(f"{FILE_REPO}/{fileName}"))
+        result = analyze_session(f"{FILE_REPO}/{fileName}")
+        return [iter.toMap() for iter in result], status.HTTP_200_OK
     except FileNotFoundError as err:
-        return "", status.HTTP_404_NOT_FOUND
+        app.logger.exception(err)
+        return "File not found.", status.HTTP_404_NOT_FOUND
+    except EmptyFileError as err:
+        app.logger.exception(err)
+        return "File is empty.", status.HTTP_404_NOT_FOUND
+    except InvalidFormatError as err:
+        app.logger.exception(err)
+        return "File has wrong format.", status.HTTP_400_BAD_REQUEST
     except Exception as err:
-        return "", status.HTTP_400_BAD_REQUEST
+        app.logger.exception(err)
+        return "", status.HTTP_500_INTERNAL_SERVER_ERROR
+
+@app.route("/file", methods = ['PUT'])
+def add_file():
+    try:
+        fileName = request.args.get("fileName")
+        fileName = fileName.replace(' ', '_')
+        if not fileName.endswith('.csv'):
+            fileName = f"{fileName}.csv"
+        
+        data = request.get_data().decode('utf-8').split('\n')
+        header_len = len(data[0].split(','))
+        with open(f"{FILE_REPO}/{fileName}", "w") as write_file:
+            writer = csv.writer(write_file)
+            for row in data:
+                values = row.split(',')
+                app.logger.error(len(values))
+                if len(values) != header_len: continue
+                writer.writerow(values)
+        return "", status.HTTP_200_OK
+    except Exception as err:
+        app.logger.exception(err)
+        return "", status.HTTP_500_INTERNAL_SERVER_ERROR
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
