40d4f16d11cec639193af425ae3d8c26d749611f
MrPuffs <thomasbeauchamp44@gmail.com>
2023-03-29 14:09:54 -0400

    see video

--- lib/widgets/dialogs/video_player_dialog.dart
+++ lib/widgets/dialogs/video_player_dialog.dart
@@ -1,22 +1,76 @@
+import 'dart:io';
+
 import 'package:figure_skating_jumps/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoPlayerDialog extends StatelessWidget {
-  const VideoPlayerDialog({super.key});
-  // Path to video : /storage/emulated/0/Movies/FigureSkatingJumpVideos/REC7363357224652569833.mp4
+class VideoPlayerDialog extends StatefulWidget {
+  const VideoPlayerDialog({Key? key}) : super(key: key);
+
+  @override
+  State<VideoPlayerDialog> createState() {
+    return _VideoPlayerDialogState();
+  }
+}
+
+class _VideoPlayerDialogState extends State<VideoPlayerDialog> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    File videoFile = File(
+        '/storage/emulated/0/Movies/FigureSkatingJumpVideos/REC7363357224652569833.mp4');
+    _controller = VideoPlayerController.file(videoFile);
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
-        backgroundColor: primaryBackground,
-        insetPadding: const EdgeInsets.only(left: 16, right: 16),
-        clipBehavior: Clip.antiAlias,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(12))),
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-              mainAxisSize: MainAxisSize.min, children: const [Text('video')]),
-        ));
+      backgroundColor: primaryBackground,
+      insetPadding: const EdgeInsets.only(left: 16, right: 16),
+      clipBehavior: Clip.antiAlias,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12))),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GestureDetector(
+            onTap: () {
+              setState(() {
+                if (_controller.value.isPlaying) {
+                  _controller.pause();
+                  return;
+                }
+                _controller.play();
+              });
+            },
+            child: FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )),
+      ),
+    );
   }
 }
