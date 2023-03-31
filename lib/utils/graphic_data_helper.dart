4f53c8c4d1b2a5287c9b5e0f5df2ea8efee72366
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-03-31 00:02:05 -0400

    working graphics but wrong order of points

--- lib/utils/graphic_data_helper.dart
+++ lib/utils/graphic_data_helper.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:figure_skating_jumps/models/graphic_data_classes/value_date_pair.dart';
 
 import '../enums/jump_type.dart';
 import '../models/capture.dart';
@@ -6,23 +7,60 @@ import '../models/jump.dart';
 
 class GraphicDataHelper {
 
+  static List<ValueDatePair> getJumpScorePerTypeGraphData(Map<String, List<Capture>> captures, JumpType type) {
+    List<ValueDatePair> graphData = [];
+    List<String> dates = getAllDates(captures);
+    for(String day in dates) {
+      double? score = getAverageJumpScorePerTypeOnDate(captures, day, type);
+      if(score != null) graphData.add(ValueDatePair(score, day));
+    }
+    return graphData;
+  }
+
   static List<String> getAllDates(Map<String, List<Capture>> captures) {
     return captures.keys.toList();
   }
 
-  static double getAverageJumpScorePerTypeOnDate(Map<String, List<Capture>> captures, String day, JumpType type) {
-    if(captures[day] == null) {
-      throw ArgumentError('captures at date was null');
+  static double? getAverageJumpScorePerTypeOnDate(Map<String, List<Capture>> captures, String day, JumpType type) {
+    if(captures[day] == null || captures[day]!.isEmpty) {
+      throw ArgumentError('captures at date was null or empty');
     }
     List<Capture> capturesOnDate = captures[day]!;
-    List<Jump> jumpsOfType = [];
+    List<Jump> jumpsOfType = _getRequiredJumpsFromCaptures(capturesOnDate, (jump) => jump.type==type);
     List<int> scoresOfJumps = [];
-    for (Capture c in capturesOnDate) {
-      jumpsOfType.addAll(c.jumps.where((jump) => jump.type==type));
-    }
     for(Jump j in jumpsOfType) {
       scoresOfJumps.add(j.score);
     }
-    return scoresOfJumps.average;
+    return scoresOfJumps.isEmpty ? null : scoresOfJumps.average;
+  }
+
+  static double getPercentageOfSuccessfulJumpsOnDate(Map<String, List<Capture>> captures, String day, JumpType type) {
+    if(captures[day] == null || captures[day]!.isEmpty) {
+      throw ArgumentError('captures at date was null or empty');
+    }
+    List<Capture> capturesOnDate = captures[day]!;
+    List<Jump> jumpsOfDate = _getRequiredJumpsFromCaptures(capturesOnDate);
+    // Jumps are considered a success if the score is one or more.
+    List<Jump> succeededJumps = jumpsOfDate.where((element) => element.score > 0).toList();
+    return succeededJumps.length / (jumpsOfDate.length * 100.0);
+  }
+
+  static double getAverageJumpDurationOnDate(Map<String, List<Capture>> captures, String day, JumpType type) {
+    if(captures[day] == null || captures[day]!.isEmpty) {
+      throw ArgumentError('captures at date was null or empty');
+    }
+    List<Capture> capturesOnDate = captures[day]!;
+    List<Jump> jumpsOfDate = _getRequiredJumpsFromCaptures(capturesOnDate);
+    // Jumps are considered a success if the score is one or more.
+    List<Jump> succeededJumps = jumpsOfDate.where((element) => element.score > 0).toList();
+    return succeededJumps.length / (jumpsOfDate.length * 100.0);
+  }
+
+  static List<Jump> _getRequiredJumpsFromCaptures(List<Capture> captures, [bool Function(Jump)? test]) {
+    List<Jump> jumps = [];
+    for (Capture c in captures) {
+      jumps.addAll(test == null ? c.jumps : c.jumps.where(test));
+    }
+    return jumps;
   }
 }
