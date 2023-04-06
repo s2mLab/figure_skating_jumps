f9817109b016932ff3aeda5c063f9199016c1d0e
Christophe St-Georges <cstg23@hotmail.ca>
2023-04-06 14:35:33 -0500

    Made bluetooth event permissions go up to flutter

--- android/app/src/main/kotlin/com/example/figure_skating_jumps/permissions/BluetoothPermissionRequest.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/permissions/BluetoothPermissionRequest.kt
@@ -1,3 +1,3 @@
 package com.example.figure_skating_jumps.permissions
 
-data class BluetoothPermissionRequest(val requestType: BluetoothRequestType, val isAccepted: Boolean )
+data class BluetoothPermissionRequest(val isAccepted: Boolean, val shouldShowDialog: Boolean )
