ee4a6564addde6da3b8e75831d7ad60040b11366
Christophe St-Georges <cstg23@hotmail.ca>
2023-04-06 09:22:21 -0500

    Removed unused metohds

--- lib/utils/time_converter.dart
+++ lib/utils/time_converter.dart
@@ -12,17 +12,4 @@ class TimeConverter {
 
     return "${nbSeconds.toString()}.${ms.toString().padLeft(3, "0")}s";
   }
-
-  static String twoDigitsPadLeft(int n) {
-    return n.toString().padLeft(2, "0");
-  }
-
-  static String _hourMinuteSecondString(int hours, int minutes, int seconds) {
-    if (hours > 0) {
-      return "${hours.toString()}h ${minutes.toString().padLeft(2, "0")}m ${seconds.toString().padLeft(2, "0")}s";
-    } else if (minutes > 0) {
-      return "${minutes.toString()}m ${seconds.toString().padLeft(2, "0")}s";
-    }
-    return "${seconds.toString()}s";
-  }
 }
