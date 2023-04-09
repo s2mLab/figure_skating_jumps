7ee5631c18c692a202257ce3ade3858325b6ee34
Thomas Beauchamp <thomasbeauchamp44@gmail.com>
2023-04-09 12:16:04 -0400

    Update jump_type.dart

--- lib/enums/jump_type.dart
+++ lib/enums/jump_type.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 enum JumpType {
   axel("A", Color.fromARGB(255, 235, 134, 255)),
-  flip("F", Color.fromARGB(255, 119, 226, 226)),
-  loop("Lo", Color.fromARGB(255, 255, 134, 134)),
-  lutz("Lu", Color.fromARGB(255, 134, 136, 255)),
   salchow("S", Color.fromARGB(255, 255, 188, 134)),
   toeLoop("T", Color.fromARGB(255, 128, 221, 151)),
+  loop("Lo", Color.fromARGB(255, 255, 134, 134)),
+  flip("F", Color.fromARGB(255, 119, 226, 226)),
+  lutz("Lz", Color.fromARGB(255, 134, 136, 255)),
   unknown(
       "-",
       Color.fromARGB(255, 173, 173,
