1b7092ed0fcb68fb2d66e96c2e15f7321070c0b1
Jimmy Bell <jimmy.bell@polymtl.ca>
2023-04-12 10:51:29 -0400

    no known devices reactive

--- lib/widgets/layout/connection_dot_view/no_known_devices.dart
+++ lib/widgets/layout/connection_dot_view/no_known_devices.dart
@@ -1,4 +1,5 @@
 import 'package:figure_skating_jumps/enums/x_sens_device_state.dart';
+import 'package:figure_skating_jumps/utils/reactive_layout_helper.dart';
 import 'package:figure_skating_jumps/widgets/icons/x_sens_state_icon.dart';
 import 'package:flutter/cupertino.dart';
 import '../../../constants/colors.dart';
@@ -10,18 +11,19 @@ class NoKnownDevices extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 64.0),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: ReactiveLayoutHelper.getWidthFromFactor(64, true)),
         child: Text(
           noConnectionMessage,
           textAlign: TextAlign.center,
           style: TextStyle(
             color: discreetText,
+            fontSize: ReactiveLayoutHelper.getHeightFromFactor(16)
           ),
         ),
       ),
       Container(
-          margin: const EdgeInsets.all(16),
+          margin: EdgeInsets.all(ReactiveLayoutHelper.getHeightFromFactor(16, true)),
           child: const XSensStateIcon(false, XSensDeviceState.reconnecting))
     ]);
   }
