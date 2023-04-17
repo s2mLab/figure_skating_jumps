029613b8f9e5cbb3dd6cfd41eb9cb46f6c2c112d
Christophe St-Georges <cstg23@hotmail.ca>
2023-04-17 12:09:50 -0500

    Documented the event channel stream handler

--- android/app/src/main/kotlin/com/example/figure_skating_jumps/channels/event_channels/XSensDotRecordingStreamHandler.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/channels/event_channels/XSensDotRecordingStreamHandler.kt
@@ -2,7 +2,16 @@ package com.example.figure_skating_jumps.channels.event_channels
 
 import com.example.figure_skating_jumps.channels.events.RecordingEvent
 
+/**
+ * A class that implements [XSensDotEventStreamHandler]
+ * to handle recording stream events
+ */
 object XSensDotRecordingStreamHandler : XSensDotEventStreamHandler<RecordingEvent>() {
+    /**
+     * Send recording events to the flutter project
+     *
+     * @param event The event to send
+     */
     override fun sendEvent(event: RecordingEvent) {
         handler.post {
             sink?.success(event.toString())
