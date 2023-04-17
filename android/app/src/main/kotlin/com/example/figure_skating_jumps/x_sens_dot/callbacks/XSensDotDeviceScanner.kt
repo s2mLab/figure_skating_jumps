c3e1cfd0cda80ad77e6cf2b6495b87ec2cba9563
Christophe St-Georges <cstg23@hotmail.ca>
2023-04-17 17:12:23 -0500

    Documented the x_sens_dot package

--- android/app/src/main/kotlin/com/example/figure_skating_jumps/x_sens_dot/callbacks/XSensDotDeviceScanner.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/x_sens_dot/callbacks/XSensDotDeviceScanner.kt
@@ -8,21 +8,36 @@ import android.content.Intent
 import android.util.Log
 import com.example.figure_skating_jumps.MainActivity
 import com.example.figure_skating_jumps.channels.event_channels.XSensDotScanStreamHandler
+import com.xsens.dot.android.sdk.interfaces.XsensDotDeviceCallback
 import com.xsens.dot.android.sdk.interfaces.XsensDotScannerCallback
 import com.xsens.dot.android.sdk.utils.XsensDotScanner
 
+/**
+ * A class that implements the [XsensDotScannerCallback] abstract class. This class allows to start,
+ * stop and handle a XSens Dot bluetooth scan
+ *
+ * @param mainActivity The application MainActivity
+ */
 class XSensDotDeviceScanner(mainActivity: MainActivity) : XsensDotScannerCallback {
     private var mainActivity: MainActivity? = mainActivity
     private var mXsScanner: XsensDotScanner = XsensDotScanner(mainActivity, this)
 
+    /**
+     * Overrides the XSens Dot scanned callback. Sends the scanned device to the flutter project
+     *
+     * @param device The scanned XSens Dot
+     * @param rssi The scanned device RSSI
+     */
     override fun onXsensDotScanned(device: BluetoothDevice?, rssi: Int) {
         if (device?.address != null) {
                 XSensDotScanStreamHandler.sendEvent(device)
         }
     }
 
+    /**
+     * Starts a XSens Dot bluetooth scan
+     */
     fun startScan() {
-        //TODO remove when requesting bluetooth permissions
         val activity = mainActivity?.context as Activity
 
         val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
@@ -37,6 +52,9 @@ class XSensDotDeviceScanner(mainActivity: MainActivity) : XsensDotScannerCallbac
         mXsScanner.startScan()
     }
 
+    /**
+     * Stops a XSens Dot bluetooth scan
+     */
     fun stopScan() {
         mXsScanner.stopScan()
     }
--- android/app/src/main/kotlin/com/example/figure_skating_jumps/x_sens_dot/enums/RecordingStatus.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/x_sens_dot/enums/RecordingStatus.kt
@@ -1,5 +1,10 @@
 package com.example.figure_skating_jumps.x_sens_dot.enums
 
+/**
+ * A enum class that contains the XSens Dot recording status
+ *
+ * @param status The recording status
+ */
 enum class RecordingStatus(val status: String) {
     SetRate("SetRate"),
     EnableRecordingNotificationDone("EnableRecordingNotificationDone"),
