5e2340f59223fec0ad81df7cd10edb0cf1904bf3
Christophe St-Georges <cstg23@hotmail.ca>
2023-04-17 13:53:48 -0500

    Commented the channel enums

--- android/app/src/main/kotlin/com/example/figure_skating_jumps/channels/enums/EventChannelParameters.kt
+++ android/app/src/main/kotlin/com/example/figure_skating_jumps/channels/enums/EventChannelParameters.kt
@@ -3,6 +3,12 @@ package com.example.figure_skating_jumps.channels.enums
 import com.example.figure_skating_jumps.channels.event_channels.*
 import io.flutter.plugin.common.EventChannel.StreamHandler
 
+/**
+ * A enum class that contains the event channel parameters
+ *
+ * @param channelName The name to identify the event channel
+ * @param streamHandler The event channel stream handler
+ */
 enum class EventChannelParameters(val channelName: String, val streamHandler: StreamHandler) {
     BluetoothChannel("xsens-dot-bluetooth-permission", BluetoothPermissionStreamHandler),
     ConnectionChannel("xsens-dot-connection", XSensDotConnectionStreamHandler),
