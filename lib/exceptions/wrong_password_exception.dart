d6772d4fc25c62caf7a1ff2488bb79dd4f7f49f9
cravethedave <d.saikali@live.com>
2023-04-17 21:17:36 -0400

    Exceptions documented

--- lib/exceptions/wrong_password_exception.dart
+++ lib/exceptions/wrong_password_exception.dart
@@ -1,6 +1,7 @@
 import 'package:figure_skating_jumps/constants/lang_fr.dart';
 import 'package:figure_skating_jumps/exceptions/abstract_ice_exception.dart';
 
+/// Exception indicating the given password was wrong and the action was refused
 class WrongPasswordException implements AbstractIceException {
   @override
   String get devMessage {
