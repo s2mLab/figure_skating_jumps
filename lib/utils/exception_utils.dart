018d2d53360cb4ab1db4196d0497b4e3a2c00584
MrPuffs <thomasbeauchamp44@gmail.com>
2023-04-17 21:19:15 -0400

    Update exception_utils.dart

--- lib/utils/exception_utils.dart
+++ lib/utils/exception_utils.dart
@@ -10,12 +10,7 @@ class ExceptionUtils {
   /// Handles a [FirebaseAuthException] by mapping its error code to a custom exception type and throwing it.
   ///
   /// Exceptions:
-  /// - Throws a [ConflictException] if the Firebase auth error code is "email-already-in-use".
-  /// - Throws an [InvalidEmailException] if the Firebase auth error code is "invalid-email".
-  /// - Throws a [WeakPasswordException] if the Firebase auth error code is "weak-password".
-  /// - Throws a [NullUserException] if the Firebase auth error code is "user-not-found".
-  /// - Throws a [WrongPasswordException] if the Firebase auth error code is "wrong-password".
-  /// - Throws a [TooManyAttemptsException] if the Firebase auth error code is "too-many-requests".
+  /// - Throws a [FirebaseAuthException] if the Firebase auth error.
   ///
   /// Parameters:
   /// - [e]: The [FirebaseAuthException] to handle.
