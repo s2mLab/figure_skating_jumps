a21c3ec81791e11a4781ee4d46e2d935ea247e0f
Pariterre <pariterre@hotmail.com>
2023-05-29 18:04:33 -0400

    Finding xsens unit!

--- ios/Plugins/Channels/EventChannels/XSensDotScanStreamHandler.h
+++ ios/Plugins/Channels/EventChannels/XSensDotScanStreamHandler.h
@@ -2,6 +2,10 @@
 #import <Flutter/Flutter.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 
-@interface XSensDotScanStreamHandler : NSObject<FlutterStreamHandler>
+#import <XsensDotSdk/XsensDotConnectionManager.h>
 
+@interface XSensDotScanStreamHandler : NSObject<FlutterStreamHandler, XsensDotConnectionDelegate>
+@property (nonatomic, copy) void (^callback)(void);
+@property (strong, nonatomic) NSMutableArray<XsensDotDevice *> *deviceList;
+@property (strong, nonatomic) NSMutableArray *connectList;
 @end
--- ios/Plugins/Channels/EventChannels/XSensDotScanStreamHandler.m
+++ ios/Plugins/Channels/EventChannels/XSensDotScanStreamHandler.m
@@ -4,39 +4,79 @@
     FlutterEventSink _eventSink;
   }
 
-- (FlutterError*)onListenWithArguments:(id)arguments
-                             eventSink:(FlutterEventSink)eventSink {
-    _eventSink = eventSink;
-    //   [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
-    //   [self sendBatteryStateEvent];
-    //   [[NSNotificationCenter defaultCenter]
-    //    addObserver:self
-    //       selector:@selector(onBatteryStateDidChange:)
-    //           name:UIDeviceBatteryStateDidChangeNotification
-    //         object:nil];
+- (id)init {
+    self = [super init];
+    if (self){
+        self.deviceList = [NSMutableArray arrayWithCapacity:20];
+        /// Set xsensDot connection delete
+        [XsensDotConnectionManager setConnectionDelegate:self];
+        /// Add notifications
+        [self addObservers];
+    }
+    return self;
+}
+
+- (FlutterError*)onListenWithArguments:(id)listener
+                             eventSink:(FlutterEventSink)emitter {
+       _eventSink = emitter;
     
-//    // Get callback id
-//        
-//        // Prepare a timer like self calling task
-//        void (^callback)(void) = ^() {
-//            int time = (int) CFAbsoluteTimeGetCurrent();
-//            
-//            eventSink([NSString stringWithFormat:@"Hello Listener! %d", time]);
-//            
-//            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), callback);
-//            
-//        };
-//        
-//        // Run task
-//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), callback);
-      return nil;
+        [self.deviceList removeAllObjects];
+        /// Start scan
+        [XsensDotConnectionManager scan];
     
+        return nil;
+}
+
+/// Add notifications
+- (void)addObservers
+{
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self selector:@selector(onDeviceBatteryUpdated:) name:kXsensDotNotificationDeviceBatteryDidUpdate object:nil];
+    [center addObserver:self selector:@selector(onDeviceTagRead:) name:kXsensDotNotificationDeviceNameDidRead object:nil];
+}
+
+/// Discovered XsensDot device
+/// @param device XsensDotDevice
+- (void)onDiscoverDevice:(XsensDotDevice *)device
+{
+    NSInteger index = [self.deviceList indexOfObject:device];
+    if(index == NSNotFound)
+    {
+        if(![self.deviceList containsObject:device])
+        {
+            [self.deviceList addObject:device];
+        }
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        @try {
+            if (self->_eventSink != nil) {
+                NSString *eventString = [NSString stringWithFormat:@"%@,%@", device.macAddress, device.peripheralName];
+                self->_eventSink(eventString);
+            }
+        } @catch (NSException *exception) {
+            if (self->_eventSink != nil) {
+                NSDictionary *errorDetails = @{
+                    NSLocalizedDescriptionKey: exception.reason,
+                    @"NSStackTraceKey": exception.callStackSymbols
+                };
+                FlutterError *error = [FlutterError errorWithCode:@"security" message:exception.reason details:errorDetails];
+                self->_eventSink(error);
+            }
+        }
+    });
+    
+    
+//
+//    self->_eventSink([NSString stringWithFormat:@"Coucou Listener!"]);
+       
 }
 
 - (FlutterError*)onCancelWithArguments:(id)arguments {
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
-  _eventSink = nil;
-  return nil;
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    _eventSink = nil;
+    self.callback = nil;
+    return nil;
 }
 
 - (void)sendEvent:(NSString*)event {
